75f5f79c2e758da1d22f6af056b6bdcf07ef3360
Vitinho369 <vitinho21.2005@gmail.com>
2024-08-14 15:12:35 -0300

    Commit inicial

---
+++ src/main/java/com/eaj/ufrn/PumpSuplementos/PumpSuplementosApplication.java
@@ -0,0 +1,13 @@
+package com.eaj.ufrn.PumpSuplementos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumpSuplementosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PumpSuplementosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eaj/ufrn/PumpSuplementos/PumpSuplementosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eaj.ufrn.PumpSuplementos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumpSuplementosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
