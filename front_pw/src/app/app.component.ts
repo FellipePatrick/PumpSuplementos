72d18d64ad69cbed3e80d84ab790193dc9be0c31
joaovitor73 <joao.vieira.712@ufrn.edu.br>
2024-08-21 15:19:36 -0300

    primeiro código front

---
+++ front_pw/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MatButtonModule, MatCardModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'front_pw';
+}
