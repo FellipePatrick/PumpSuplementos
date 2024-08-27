b2155eb92030df094da01864713eaa5085d3030c
Vitinho369 <vitinho21.2005@gmail.com>
2024-08-26 22:03:41 -0300

    Ajustando delete de suplemento

--- demo/src/main/java/com/example/demo/dto/SuplementoRequestDTO.java
+++ demo/src/main/java/com/example/demo/dto/SuplementoRequestDTO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 public class SuplementoRequestDTO {
     private String nome;
     private int quantidade;
- //   private String imageUri;
+    private String imageUri;
     private float preco;
     private String descricao;
     private String categoria;
